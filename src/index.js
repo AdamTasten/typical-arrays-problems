57c9ffbe4c158e46a1a339fac9dadf618cba743c
Adam Zdzeshchyts <adamchic2@tut.by>
2021-02-18 21:30:11 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined) return 0;
+  if (array.toString() === "") return 0;
+  let minimal = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < minimal) minimal = array[i];
+  }
+  return minimal;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined) return 0;
+  if (array.toString() === "") return 0;
+  let maximal = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > maximal) maximal = array[i];
+  }
+  return maximal;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined) return 0;
+  if (array.toString() === "") return 0;
+  let average = 0;
+  for (let i = 0; i < array.length; i++) {
+    average += array[i];
+  }
+  return average / array.length;
 }
